75a0b50c72d2de304d24bca0d027e87308a42b25
Alessandro Rocchi <alessandro.rocchi13@studio.unibo.it>
2026-01-27 23:27:34 +0100

    add button logic

---
+++ scripts/scripts.js
@@ -0,0 +1,49 @@
+const font_list = [
+        'Arial', 
+        'Courier New', 
+        'Georgia', 
+        'Impact', 
+        'Times New Roman', 
+        'Verdana', 
+        'Trebuchet MS',
+        'Comic Sans MS',
+        'Lucida Console',
+        'Palatino Linotype',
+        'Tahoma',
+        'Garamond',
+        'Bookman',
+        'Candara',
+        'Calibri',
+        'Futura',
+        'Helvetica',
+        'Optima',
+        'Didot',
+        'Rockwell'
+    ];
+
+const phrase1 = document.getElementById('phrase1');
+const phrase2 = document.getElementById('phrase2');
+const phrase3 = document.getElementById('phrase3');
+const button = document.getElementById('btnShuffle');
+
+function Randomfont(){
+    const randomFont1 = font_list[Math.floor(Math.random() * font_list.length)];
+    const randomFont2 = font_list[Math.floor(Math.random() * font_list.length)];
+    const randomFont3 = font_list[Math.floor(Math.random() * font_list.length)];
+    phrase1.style.fontFamily = randomFont1;
+    phrase2.style.fontFamily = randomFont2;
+    phrase3.style.fontFamily = randomFont3;
+}
+
+button.addEventListener('click', Randomfont);
+
+window.addEventListener('load', () => {
+    button.click();
+});
+
+window.addEventListener('keydown', (event) => {
+    if (event.key === '32' || event.code === 'Space') {
+        event.preventDefault(); 
+        button.click();
+    }
+});
