c3306d5d57946c44180c8a02b13cb332b4016bc7
Alessandro Rocchi <alessandro.rocchi13@studio.unibo.it>
2026-01-28 11:35:51 +0100

    refined scripts and index

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,4 +1,4 @@
-const font_list= [
+/*const font_list= [
         'Arial', 
         'Courier New', 
         'Georgia', 
@@ -19,17 +19,36 @@ const font_list= [
         'Optima',
         'Didot',
         'Rockwell'
-    ];
+    ];*/
 
 const phrase1 = document.getElementById('phrase1');
 const phrase2 = document.getElementById('phrase2');
 const phrase3 = document.getElementById('phrase3');
 const button = document.getElementById('btnShuffle');
+let arrayDati = [];
+
+async function caricaDati() {
+  try {
+    const risposta = await fetch('./font_database/font.json');
+    
+    arrayDati = await risposta.json();
+
+    Randomfont();
+    
+  } catch (errore) {
+    console.error("Errore nel caricamento:", errore);
+  }
+}
+
 
 function Randomfont(){
-    const randomFont1 = font_list[Math.floor(Math.random() * font_list.length)];
-    const randomFont2 = font_list[Math.floor(Math.random() * font_list.length)];
-    const randomFont3 = font_list[Math.floor(Math.random() * font_list.length)];
+    if (arrayDati.length === 0) {
+        console.error("L'array dei dati è vuoto. Assicurati che i dati siano stati caricati correttamente.");
+        return;
+    }
+    const randomFont1 = arrayDati[Math.floor(Math.random() * arrayDati.length)];
+    const randomFont2 = arrayDati[Math.floor(Math.random() * arrayDati.length)];
+    const randomFont3 = arrayDati[Math.floor(Math.random() * arrayDati.length)];
     phrase1.style.fontFamily = randomFont1;
     phrase2.style.fontFamily = randomFont2;
     phrase3.style.fontFamily = randomFont3;
@@ -37,13 +56,14 @@ function Randomfont(){
 
 button.addEventListener('click', Randomfont);
 
-window.addEventListener('load', () => {
-    button.click();
-});
-
 window.addEventListener('keydown', (event) => {
     if (event.key === '32' || event.code === 'Space') {
         event.preventDefault(); 
         button.click();
     }
+});
+
+document.addEventListener('DOMContentLoaded', () => {
+    console.log("Pagina caricata, avvio il caricamento dati...");
+    caricaDati();
 });
